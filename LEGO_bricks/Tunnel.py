5e6f6190b502ba4780dc09fa52e3d235fc97c1a4
Gabriel Fleischer <gab.fleischer@gmail.com>
2018-01-10 19:05:20 +0100

    Updated Tunnel.py

--- LEGO_bricks/Tunnel.py
+++ LEGO_bricks/Tunnel.py
@@ -6,28 +6,30 @@ from mcpi import minecraft
 
 mc = minecraft.Minecraft.create()
 
-def tunnel(width, length):
+def tunnel(width, height, length):
 	
 	"""
 	Create a tunnel at the player's current position in the direction he's facing :
-		- width : The width of the tunnel
+		- width : The width of the tunnel (from the center to the side)
+		- height : The height of the tunnel
 		- length : The length of the tunnel
 	"""
 	
 	px, py, pz = mc.player.getPos()
 	
 	angle = mc.player.getPos().getRotation()
-	
-	if angle >= 315 or angle <= 45 :
-		for i in range(length):
-			
-	elif angle >= 45 and angle <= 135:
-		for i in range(length):
-			
-	elif angle >= 135 and angle <= 225:
-		for i in range(length):
-			
-	elif angle >= 225 and angle <= 315:
-		for i in range(length):
-			
+	#Length
+	for i in range(length):
+		#width
+		for j in range(width*2+1):
+			#Height
+			for y in range(height):
+				if angle >= 315 or angle <= 45 :
+					setBlock(px+i, py+y, pz+j-width, 0)
+				elif angle >= 45 and angle <= 135:
+					setBlock(px+j-width, py+y, pz+i, 0)
+				elif angle >= 135 and angle <= 225:
+					setBlock(px-i, py+y, pz+j-width, 0)
+				elif angle >= 225 and angle <= 315:
+					setBlock(px+j-width, py+y, pz-i, 0)
 tunnel(2, 15)

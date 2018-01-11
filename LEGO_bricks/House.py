bd091664d141d79d574d0489864d9c36c705fbde
Gabriel Fleischer <gabriel.fleischer@bugnon.educanet2.ch>
2018-01-11 15:13:58 +0100

    Updated files

--- LEGO_bricks/House.py
+++ LEGO_bricks/House.py
@@ -36,7 +36,7 @@ def house(length, height, width, wallBlock, groundBlock, roofBlock):
 	mc.setBlock(int(px + float(length)/2), py+2, pz, 64, 0)
 	
 	#window
-	mc.setBlock(px, py+2, int(pz+float(width)/2), 102)
+	mc.setBlocks(px, py+2, int(pz+float(width)/2)-1, px, py+2, int(pz+float(width)/2)+1, 102)
 	
 	#roof
 	for i in range(width+2):
@@ -49,7 +49,7 @@ def house(length, height, width, wallBlock, groundBlock, roofBlock):
 			if x < float(length)/2:
 				h=h+1
 			if x >=1 and x <= length+1:
-				mc.setBlocks(px+x, py+height+1, pz+z, px+x, py+height+h, pz+z, wallBlock)
+				mc.setBlocks(px+x, py+height, pz+z, px+x, py+height+h, pz+z, wallBlock)
 			
 			mc.setBlock(px+x, py+height+h, pz+z, roofBlock)
 	

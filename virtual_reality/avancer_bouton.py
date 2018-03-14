deb944dee6b21776b060553f447c0e4ccd4acf78
BenJonas <benoitgallois@bugnon.educanet2.ch>
2018-03-14 19:28:47 +0000

    hey

--- virtual_reality/avancer_bouton.py
+++ virtual_reality/avancer_bouton.py
@@ -1,17 +1,19 @@
-import RPi.GPIO as gpio
+##import RPi.GPIO as gpio
 import time
+import pyautogui
 from mcpi.minecraft import Minecraft
+import RPi.GPIO as gpio
 mc = Minecraft.create()
 x, y, z = mc.player.getPos()
 
-buttonL = 18
-buttonR = je sais pas
+buttonL = 14
+buttonR = 15
 
 
 gpio.setmode(gpio.BCM)
 gpio.setup(buttonL, gpio.IN, pull_up_down=gpio.PUD_UP)
 gpio.setup(buttonR, gpio.IN, pull_up_down=gpio.PUD_UP)
-
+    
 left0 = False
 right0 = False
 
@@ -23,18 +25,22 @@ while True:
 
 	if not left and left0:
 		mc.postToChat("pushed left")
+		pyautogui.keyDown('w')
 	if left and not left0:
 		mc.postToChat("released left")
-		mc.player.setPos(x+5, y, z)
+		pyautogui.keyUp('w')
+		
 
 	left0 = left
 
 	if not right and right0:
 		mc.postToChat("pushed right")
-	if right and not left0:
+		pyautogui.keyDown('w')
+	if right and not right0:
 		mc.postToChat("realeased right")
-		mc.player.setPos(x+5, y, z)
+		#pyautogui.press('w')
+		pyautogui.keyUp('w')
+		
+		
 	right0 = right
-
-
 

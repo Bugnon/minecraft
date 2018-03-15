a78c45720c953619d5867655db4dc408f7e6cc52
BenJonas <benoitgallois@bugnon.educanet2.ch>
2018-03-15 15:24:30 +0000

    hey

--- virtual_reality/casser_brique.py
+++ virtual_reality/casser_brique.py
@@ -8,7 +8,6 @@ x, y, z = mc.player.getPos()
 buttonL = 14
 buttonR = 15
 
-
 gpio.setmode(gpio.BCM)
 gpio.setup(buttonL, gpio.IN, pull_up_down=gpio.PUD_UP)
 gpio.setup(buttonR, gpio.IN, pull_up_down=gpio.PUD_UP)
@@ -21,21 +20,10 @@ while True:
     right = gpio.input(buttonR)
 
     x,y,z = mc.player.getPos()
-
-    if not left and left0:
-	    pyautogui.keyDown('w')
+    
     if left and not left0:
-	    pyautogui.keyUp('w')
-		
+        pyautogui.click()
+        
+    left = left0
 
-    left0 = left
-
-    if not right and right0:
-	    pyautogui.keyDown('w')
-    if right and not right0:
-	    pyautogui.keyUp('w')
-		
-		
-    right0 = right
-
-
+    

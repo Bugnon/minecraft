c5de2ef34499fe83d86d9470bff4539903636433
Samuel Gachon <samuelgachon01@gmail.com>
2018-03-22 14:15:39 +0000

    updated ferry wheel and added bumper cars

--- amusement_park/park.py
+++ amusement_park/park.py
@@ -23,8 +23,19 @@ def ferry_wheel(r):
         mc.setBlock(x,s2,z+11,block.IRON_BLOCK.id)
         s2 = s2-1
         socle2 = mc.getBlock(x,s2,z+11)
-        
-        
+    mc.setBlock(x,y+r+5,z+10,block.IRON_BLOCK.id)
+    
+def bumper_cars(L,l) :
+    x,y,z = mc.player.getPos()
+    mc.setBlocks(x,y-1,z+10,x+l,y-1,z+10+L,block.IRON_BLOCK.id)
+    mc.setBlocks(x,y+4,z+10,x+l,y,z+10+L,0)
+    mc.setBlocks(x,y,z+10,x,y+4,z+10,block.FENCE.id)
+    mc.setBlocks(x,y,z+10+L,x,y+4,z+10+L,block.FENCE.id)
+    mc.setBlocks(x+l,y,z+10,x+l,y+4,z+10,block.FENCE.id)
+    mc.setBlocks(x+l,y,z+10+L,x+l,y+4,z+10+L,block.FENCE.id)
+    mc.setBlocks(x,y+5,z+10,x+l,y+5,z+10+L,block.IRON_BLOCK.id)
+    
+
         
 
                         

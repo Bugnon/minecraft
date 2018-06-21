6e6e1e15cd0b869428ce66a3c00dfd7ce71ade00
Gabriel Fleischer <34675235+gabi4111@users.noreply.github.com>
2018-06-21 10:04:57 +0200

    Removed something that wasn't supposed to be there

--- LEGO_bricks/LEGO_Bricks.py
+++ LEGO_bricks/LEGO_Bricks.py
@@ -350,7 +350,7 @@ def onButtonPressed():
 
 
 def takePicture(frame, M):
-    """Applique les transformations sur l'image, voir frame_data
+    """Applique les transformations sur l'image
 """
     global final_size
     # Application de l'algorithme de perspective

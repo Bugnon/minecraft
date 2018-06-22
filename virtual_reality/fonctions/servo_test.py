252e793f6ac7e4135f3e3cf861183b4e4d8b6a9b
Samuel Gachon <samuelgachon01@gmail.com>
2018-06-22 15:39:56 +0000

    Merge branch 'master' of https://github.com/Bugnon/minecraft

---
+++ virtual_reality/fonctions/servo_test.py
@@ -0,0 +1,44 @@
+import time
+import pyautogui
+import RPi.GPIO as gpio
+
+
+#Installations boutons
+buttonL = 14
+buttonR = 15
+
+
+gpio.setmode(gpio.BCM)
+gpio.setup(buttonL, gpio.IN, pull_up_down=gpio.PUD_UP)
+gpio.setup(buttonR, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+left0 = False
+right0 = False
+
+#Installations Servo
+
+gpio.setup(2, gpio.OUT)
+
+pwm = gpio.PWM(2, 50)
+
+
+
+
+pwm.start(7.5)
+
+
+try:
+        while True:
+                pwm.ChangeDutyCycle(2.5)
+                time.sleep(1)
+                pwm.ChangeDutyCycle(5) 
+                time.sleep(1)
+                pwm.ChangeDutyCycle(7.5) 
+                time.sleep(1)
+                pwm.ChangeDutyCycle(10)
+                time.sleep(1)
+                pwm.ChangeDutyCycle(12.5)
+                time.sleep(1)
+except KeyboardInterrupt:
+        GPIO.cleanup()
+
